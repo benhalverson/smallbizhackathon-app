5ce5cfdb34896546fa056f3058f68ae10cb81032
Ben Halverson <benhalverson33@gmail.com>
2018-11-03 14:01:32 -0700

    feat(search): code ...

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,5 +1,72 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-const SearchComponent = () => <div>search component</div>;
+import PropTypes from 'prop-types';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+// import ReactDOM from 'react-dom';
+// import AppBar from '@material-ui/core/AppBar';
+// import Menu from '@material-ui/core/Menu';
+// import Toolbar from '@material-ui/core/Toolbar';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  }
+});
+class SearchComponent extends React.Component {
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null,
+    left: false
+    // open: false,
+  };
 
-export default SearchComponent;
+  render() {
+    const { classes, theme } = this.props;
+    return (
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <Input
+          placeholder="Search…"
+          disableUnderline
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchComponent);
